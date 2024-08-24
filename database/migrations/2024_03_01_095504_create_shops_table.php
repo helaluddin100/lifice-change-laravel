336ed48876b4f18393dbc90ed101397e89c87ea3
Helal Uddin <freelancerhelaluddin@gmail.com>
2024-08-25 02:53:21 +0600

    shop database update

--- database/migrations/2024_03_01_095504_create_shops_table.php
+++ database/migrations/2024_03_01_095504_create_shops_table.php
@@ -20,17 +20,55 @@ return new class extends Migration
             $table->string('shop_type');
             $table->string('email');
             $table->string('number');
-            $table->string('country')->nullable();
             $table->decimal('balance', 8, 2)->nullable();
             $table->string('address')->nullable();
             $table->text('details')->nullable();
             $table->string('shop_url')->unique();
-            $table->string('stock_management')->nullable();
+            $table->string('logo')->nullable();
+            $table->string('color')->nullable();
+
+            $table->boolean('stock_management')->default(false);
             $table->string('vat_tax')->nullable();
+
+            $table->string('top_announcement')->nullable();
+            $table->string('visitors')->default(0)->nullable();
+
+            //location
+            $table->string('country')->nullable();
+            $table->string('division')->nullable();
+            $table->string('district')->nullable();
+            $table->string('city')->nullable();
+            $table->string('zip_code')->nullable();
+
+            //delivery charge policy
             $table->decimal('default_delivery_charge', 8, 2)->nullable();
             $table->json('specific_delivery_charges')->nullable();
             $table->text('payment_message')->nullable();
             $table->text('delivery_charge_note')->nullable();
+
+            // GTM and Facebook Pixel
+            $table->string('gtm_id')->nullable();
+            $table->string('facebook_pixel_id')->nullable();
+            $table->string('facebook_pixel_access_token')->nullable();
+            $table->string('facebook_pixel_event')->nullable();
+
+            // Chat Support
+            $table->boolean('live_chat')->default(false);
+            $table->boolean('whatsapp_chat')->default(false);
+            $table->string('live_chat_whatsapp')->nullable();
+
+            // shop domain
+            $table->string('shop_domain')->nullable();
+            $table->string('subdomain_id')->nullable();
+            $table->string('shop_subdomain_name')->nullable();
+
+            // shop domain
+            $table->string('shop_domain_name')->nullable();
+
+            //language    //
+            $table->string('language')->nullable();
+
+            //social media
             $table->string('facebook')->nullable();
             $table->string('instagram')->nullable();
             $table->string('linkedin')->nullable();
@@ -39,13 +77,21 @@ return new class extends Migration
             $table->string('discord')->nullable();
             $table->string('telegram')->nullable();
             $table->string('whatsapp')->nullable();
-            $table->string('logo')->nullable();
-            $table->string('color')->nullable();
-            $table->string('district')->nullable();
-            $table->string('division')->nullable();
-            $table->string('city')->nullable();
-            $table->string('zip_code')->nullable();
+
+            //shop others setting
+            $table->boolean('slider')->default(0);
+            $table->boolean('today_sell')->default(0);
+            $table->boolean('new_arrival')->default(0);
+            $table->boolean('offer_product')->default(0);
+            $table->boolean('hot_deal')->default(0);
+            $table->boolean('flash_deal')->default(0);
+            $table->boolean('top_rated')->default(0);
+            $table->boolean('top_selling')->default(0);
+            $table->boolean('related_product')->default(0);
+
+            //status
             $table->boolean('status')->default(1);
+
 
             $table->timestamps();
         });
