d7d9cc3aa705a396808f4c1d6738ebc0e3c88f2f
Helal Uddin <freelancerhelaluddin@gmail.com>
2025-05-28 02:20:49 +0600

    update

---
+++ app/Http/Controllers/PaymentGetwayaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PaymentGetwaya;
+use Illuminate\Http\Request;
+
+class PaymentGetwayaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PaymentGetwaya  $paymentGetwaya
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PaymentGetwaya $paymentGetwaya)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PaymentGetwaya  $paymentGetwaya
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PaymentGetwaya $paymentGetwaya)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PaymentGetwaya  $paymentGetwaya
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PaymentGetwaya $paymentGetwaya)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PaymentGetwaya  $paymentGetwaya
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PaymentGetwaya $paymentGetwaya)
+    {
+        //
+    }
+}
---
+++ database/migrations/2025_05_28_021117_create_payment_getwayas_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_getwayas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id'); // যার গেটওয়ে সেটিং এটি তার জন্য
+            $table->string('name'); // bkash, nagad, rocket, sslcommerz
+            $table->string('getwaya_id');
+            $table->string('type')->comment('payment gateway type')->nullable(); // payment gateway type
+            $table->string('account_number')->nullable(); // মোবাইল/পেমেন্ট নাম্বার
+            $table->string('getwaya_instruction')->nullable(); // মোবাইল/পেমেন্ট নাম্বারের মালিকের নাম
+            $table->string('qr_code_image')->nullable(); // QR কোড ইমেজ (পাথ)
+
+            $table->string('api_mood')->nullable();
+            $table->string('store_id')->nullable();
+            $table->string('store_id')->nullable();
+            $table->string('store_password')->nullable();
+
+            $table->string('api_key')->nullable(); // API integration এর জন্য
+            $table->string('api_secret')->nullable(); // API integration এর জন্য
+            $table->boolean('status')->default(true); // এই গেটওয়ে কি সক্রিয়?
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_getwayas');
+    }
+};
